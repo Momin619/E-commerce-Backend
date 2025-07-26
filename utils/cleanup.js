3d8f1acc6990c53b96d42929977e84653d7b4426
momin56 <mominyt56@gmail.com>
2025-07-26 13:09:22 +0500

    testing

--- utils/cleanup.js
+++ utils/cleanup.js
@@ -3,23 +3,19 @@ const User = require("../model/User");
 const Product = require("../model/Product");
 
 const cleanUserData = async () => {
+  const allProducts = await Product.find({}, "_id");
+  const productIdSet = new Set(allProducts.map((p) => p._id.toString()));
+
   const users = await User.find();
 
   for (const user of users) {
-    const validFavourites = [];
-    const validCart = [];
+    const validFavourites = user.favourites.filter((id) =>
+      productIdSet.has(id.toString())
+    );
 
-    for (const favId of user.favourites) {
-      if (await Product.exists({ _id: favId })) {
-        validFavourites.push(favId);
-      }
-    }
-
-    for (const item of user.cart) {
-      if (await Product.exists({ _id: item.productId })) {
-        validCart.push(item);
-      }
-    }
+    const validCart = user.cart.filter((item) =>
+      productIdSet.has(item.productId?.toString())
+    );
 
     await User.updateOne(
       { _id: user._id },
